d93f4c73afae0cdb5f36788c5ea80cc0092b8f7f
Damian Trafialek <damian.trafialek@gmail.com>
2024-05-08 12:46:28 +0000

    add: devcontainer setup

---
+++ Makefile
@@ -0,0 +1,2 @@
+test-native:
+	chmod +x ./scripts/test_native.sh && ./scripts/test_native.sh
---
+++ scripts/install_platformio.sh
@@ -0,0 +1,5 @@
+pip install platformio
+
+curl -fsSL https://raw.githubusercontent.com/platformio/platformio-core/develop/platformio/assets/system/99-platformio-udev.rules | sudo tee /etc/udev/rules.d/99-platformio-udev.rules
+
+sudo service udev restart
