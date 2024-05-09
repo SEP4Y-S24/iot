0382811a858d1d0a4a6862c908d5d2c52813849c
Jan Metela <janmetela@seznam.cz>
2024-05-09 09:39:10 +0200

    IOT-1-1 moving arduino specific stuff to external_screen.c to external_screen.h

--- lib/drivers/external_screen.c
+++ lib/drivers/external_screen.c
@@ -1,6 +1,17 @@
 
 #ifndef NATIVE_TESTING
 #include "external_screen.h" /* Include the correct header file */
+#include <avr/io.h>
+#include <util/delay.h>
+
+/* LCD Port and Pin Definitions */
+#define LCD_Data_Dir DDRA    /* Define LCD data port direction */
+#define LCD_Command_Dir DDRC /* Define LCD command port direction register */
+#define LCD_Data_Port PORTA
+#define LCD_Command_Port PORTC
+#define RS PC0 /* Define Register Select pin */
+#define RW PC1 /* Define Read/Write signal pin */
+#define EN PC2 /* Define Enable signal pin */
 
 /* Send a command to the LCD */
 void external_screen_command(unsigned char cmnd)
@@ -76,7 +87,7 @@ void external_screen_two_rows(const char *row1_text, char row1_pos, const char *
 }
 
 /* Function to scroll text on the LCD for a specified number of cycles */
-void external_screen_static_with_scroll(const char *static_text, const char *scroll_text, uint16_t delay, int cycles)
+void external_screen_static_with_scroll(const char *static_text, const char *scroll_text, int delay, int cycles)
 {
     /* Display a static string on the first row */
     external_screen_string_xy(1, 0, static_text);
