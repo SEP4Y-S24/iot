f62ea00b49eed877260638bf65a0fb87b5273498
Jakub Cerovsky <jacobcerovsky@gmail.com>
2024-05-13 11:40:18 +0200

    Code refactor

--- lib/controllers/humidity_temperature.c
+++ lib/controllers/humidity_temperature.c
@@ -4,18 +4,26 @@
 #include "dht11.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+static void humidity_temperature_send()
+{
+    char request_message[16];
+    humidity_temperature_get(request_message);
+    ccp_message_sender_send_request(CCP_AT_TH, request_message);
+}
 
 void humidity_temperature_init()
 {
     // TODO: Change this to 30 minutes after testing
-    periodic_task_init_d(humidity_temperature_send(), 60000)
+    periodic_task_init_d(humidity_temperature_send, 60000);
 }
 
 void humidity_temperature_get(char *message_buffer)
 {
     uint8_t temperature_int, temperature_dec, humidity_int, humidity_dec;
     char number_stringified[8];
-    if (dht11_get(humidity_int, humidity_dec, temperature_int, temperature_dec) == DHT11_OK)
+    if (dht11_get(&humidity_int, &humidity_dec, &temperature_int, &temperature_dec) == DHT11_OK)
     {
         sprintf(number_stringified, "%d.%d", temperature_int, temperature_dec);
         strcpy(message_buffer, "T");
@@ -31,11 +39,4 @@ void humidity_temperature_get(char *message_buffer)
     {
         strcpy(message_buffer, "Error occured while getting the Humidity and Temperature data.");
     }
-}
-
-static humidity_temperature_send()
-{
-    char request_message[16];
-    humidity_temperature_get(request_message);
-    ccp_message_sender_send_request(CCP_AT_TH, request_message);
 }
