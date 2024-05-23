024cab3395f3e6a74508f5d2dcba5bc61520338e
Jakub Cerovsky <jacobcerovsky@gmail.com>
2024-05-23 10:39:41 +0200

    changing folder structure

--- scripts/test_native.sh
+++ scripts/test_native.sh
@@ -1,16 +1,16 @@
 echo "Running all native tests..."
 
 echo "Running wifi_connect_state test..."
-pio test -e native_test -f "./test/native_tests/native_test_wifi_connect_state"
+pio test -e native_test -f "native_tests/native_test_wifi_connect_state"
 
 echo "Running ccp_protocol test..."
-pio test -e native_test -f "./test/native_tests/native_test_ccp_protocol"
+pio test -e native_test -f "native_tests/native_test_ccp_protocol"
 
 echo "Running alarm test..."
-pio test -e native_test -f "./test/native_tests/native_test_alarm"
+pio test -e native_test -f "native_tests/native_test_alarm"
 
 echo "Running message_handler test..."
-pio test -e native_test -f "./test/native_tests/native_test_message_handler"
+pio test -e native_test -f "native_tests/native_test_message_handler"
 
 echo "Running auth_test test..."
-pio test -e native_test -f "./test/native_tests/native_test_auth_state"
+pio test -e native_test -f "native_tests/native_test_auth_state"
