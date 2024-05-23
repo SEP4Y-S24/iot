9b5f8ef009ded8d8eeadbbd7ab92377d53dc0d81
Jakub Cerovsky <jacobcerovsky@gmail.com>
2024-05-23 18:12:11 +0200

    Test refactor

--- scripts/test_native.sh
+++ scripts/test_native.sh
@@ -1,25 +1,25 @@
 echo "Running all native tests..."
 
 echo "Running wifi_connect_state test..."
-pio test -e native_test -f "./test/native_tests/test_native_wifi_connect_state"
+pio test -e native_test -f "native_tests/test_native_wifi_connect_state"
 
 echo "Running ccp_protocol test..."
-pio test -e native_test -f "./test/native_tests/test_native_ccp_protocol"
+pio test -e native_test -f "native_tests/test_native_ccp_protocol"
 
 echo "Running alarm test..."
-pio test -e native_test -f "./test/native_tests/test_native_alarm"
+pio test -e native_test -f "native_tests/test_native_alarm"
 
 echo "Running message_handler test..."
-pio test -e native_test -f "./test/native_tests/test_native_message_handler"
+pio test -e native_test -f "native_tests/test_native_message_handler"
 
 echo "Running periodic_request test..."
-pio test -e native_test -f "./test/native_tests/test_native_periodic_request"
+pio test -e native_test -f "native_tests/test_native_periodic_request"
 
 echo "Running humidity_temperature test..."
-pio test -e native_test_humidity_temperature -f "./test/native_tests/test_native_humidity_temperature"
+pio test -e native_test -f "native_tests/test_native_humidity_temperature"
 
 echo "Running auth_test test..."
-pio test -e native_test_auth_state -f "./test/native_tests/test_native_auth_state"
+pio test -e native_test -f "native_tests/test_native_auth_state"
 
 echo "Running encryption test..."
-pio test -e native_test_encryption -f "./test/native_tests/test_native_cryptator"
+pio test -e native_test_encryption -f "native_tests/test_native_cryptator"
